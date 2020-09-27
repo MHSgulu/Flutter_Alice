5969e82de7832bbf207bd038cdb0cec16f7143de
Ma <594408294@qq.com>
2020-09-27 10:16:30 +0800

    实时热点 更改为 热点词汇分类 完善功能

---
+++ lib/ui/one/hotspot/hot_tword_tab.dart
@@ -0,0 +1,77 @@
+import 'package:alice/common/custom_scroll_behavior.dart';
+import 'package:alice/model/hot_word_type_entity.dart';
+import 'package:alice/ui/one/hotspot/hot_word_list.dart';
+import 'package:flutter/material.dart';
+
+class HotWordTab extends StatefulWidget {
+  final String title;
+  final List<HotWordTypeResultShowapiResBodyListChildList> childList;
+
+  const HotWordTab({Key key, this.title, this.childList}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => HotWordTabState();
+}
+
+class HotWordTabState extends State<HotWordTab> {
+  List<Tab> myTabs = List();
+  List<String> typeId = List();
+
+  @override
+  void initState() {
+    widget.childList.forEach((f) {
+      typeId.add(f.id);
+      myTabs.add(Tab(
+        text: f.name,
+      ));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+          title: Text(widget.title,style: TextStyle(color: Colors.black, fontSize: 17),),
+          centerTitle: true,
+          elevation: 0.5,
+          bottom: PreferredSize(
+            child: Container(
+              height: 40,
+              color: Colors.white,
+              child: TabBar(
+                tabs: myTabs,
+                isScrollable: true,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                labelColor: Colors.black87,
+                unselectedLabelColor: Colors.black45,
+              ),
+            ),
+            preferredSize: Size.fromHeight(35),
+          ),
+        ),
+        body: ScrollConfiguration(
+            behavior: CustomScrollBehavior(false, false, null),
+            child: TabBarView(
+              children: [
+                for (final id in typeId)
+                  HotWordList(id: id,),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
