c44fe89d70ebf4ae70b690b95233805dceca4ce3
Ma <594408294@qq.com>
2020-09-30 09:49:56 +0800

    android 插件代码

---
+++ android/app/src/main/java/com/ow/alice/MainActivity.java
@@ -0,0 +1,111 @@
+package com.ow.alice;
+
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+
+  private BinaryMessenger binaryMessenger;
+  private static final String CHANNEL = "samples.flutter.io/battery";
+  private static final String CHANNEL_1 = "com.ow.alice/android-version";
+
+  private MethodChannel methodChannel;
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    binaryMessenger = Objects.requireNonNull(getFlutterEngine()).getDartExecutor().getBinaryMessenger();
+    /*methodChannel = new MethodChannel(binaryMessenger,CHANNEL_1);
+    methodChannel.setMethodCallHandler((call, result) -> {
+
+    });*/
+
+    //获取手机电量
+    new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+              @Override
+              public void onMethodCall(@NotNull MethodCall call, @NotNull MethodChannel.Result result) {
+                if (call.method.equals("getBatteryLevel")) {
+                  int batteryLevel = getBatteryLevel();
+
+                  if (batteryLevel != -1) {
+                    result.success(batteryLevel);
+                  } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null);
+                  }
+                }
+                else {
+                  //处理对未实现方法的调用。
+                  result.notImplemented();
+                }
+
+              }
+            });
+
+
+    //获取手机安卓系统版本
+    new MethodChannel(binaryMessenger,CHANNEL_1).setMethodCallHandler((call, result) -> {
+      if (call.method.equals("getAndroidVersion")){
+        result.success("Android " + android.os.Build.VERSION.RELEASE);
+      }
+      else {
+        result.notImplemented();
+      }
+    });
+
+
+
+  }
+
+
+
+  //获取手机电量 具体实现
+  private int getBatteryLevel() {
+    int batteryLevel = -1;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    } else {
+      Intent intent = new ContextWrapper(getApplicationContext()).
+              registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+              intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    }
+
+    return batteryLevel;
+  }
+
+
+
+
+
+
+
+
+
+
+}
