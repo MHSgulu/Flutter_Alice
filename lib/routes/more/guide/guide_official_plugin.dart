f5c0d2fd9d4361b9a4c6cb73911a636887d181a1
Ma <594408294@qq.com>
2020-11-27 14:02:01 +0800

    官方插件代码目录 整理

--- lib/routes/more/guide/guide_official_plugin.dart
+++ lib/routes/more/guide/guide_official_plugin.dart
@@ -1,7 +1,8 @@
-import 'package:alice/routes/example/official/example_device_info.dart';
-import 'package:alice/routes/example/official/example_image_picker.dart';
-import 'package:alice/routes/example/official/example_package_info.dart';
-import 'package:alice/routes/example/official/example_webView.dart';
+import 'package:alice/routes/example/official/plugins/example_device_info.dart';
+import 'package:alice/routes/example/official/plugins/example_image_picker.dart';
+import 'package:alice/routes/example/official/plugins/example_local_auth.dart';
+import 'package:alice/routes/example/official/plugins/example_package_info.dart';
+import 'package:alice/routes/example/official/plugins/example_webView.dart';
 import 'package:alice/widgets/custom/custom_scroll_behavior.dart';
 import 'package:alice/widgets/custom/my_appbar.dart';
 
@@ -18,6 +19,7 @@ class GuideOfficialPluginRouteState extends State<GuideOfficialPluginRoute> {
     'image_picker',
     'webview_flutter',
     'device_info',
+    'local_auth',
   ];
 
   List<String> subTitleList = [
@@ -25,20 +27,22 @@ class GuideOfficialPluginRouteState extends State<GuideOfficialPluginRoute> {
     'Flutter插件，用于从Android和iOS图像库中选择图像，并使用相机拍摄新照片',
     'Flutter插件，可在Android和iOS上提供WebView小部件',
     'Flutter插件可提供有关设备（制造商，型号等）以及运行该应用程序的Android或iOS版本的详细信息',
+    '适用于Android和iOS设备身份验证传感器的Flutter插件，例如指纹读取器和Touch ID',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(
-        label: 'Plugin',
+        label: 'flutter.dev(Plugin)',
         onPressedBack: () => Navigator.pop(context),
       ),
       body: ScrollConfiguration(
         behavior: CustomScrollBehavior(
             isShowLeading: true,
             isShowTrailing: true,
-            color: Colors.indigo[400]),
+            color: Colors.indigo[400],
+        ),
         child: ListView.builder(
           itemCount: titleList.length,
           itemBuilder: (context, index) {
@@ -68,23 +72,19 @@ class GuideOfficialPluginRouteState extends State<GuideOfficialPluginRoute> {
   void jumpToRoute(int index) {
     switch (index) {
       case 0:
-        Navigator.push(
-            context, MaterialPageRoute(builder: (_) => ExamplePackageInfo()));
+        Navigator.push(context, MaterialPageRoute(builder: (_) => ExamplePackageInfo()));
         break;
       case 1:
-        Navigator.push(
-            context, MaterialPageRoute(builder: (_) => ExampleImagePicker()));
+        Navigator.push(context, MaterialPageRoute(builder: (_) => ExampleImagePicker()));
         break;
       case 2:
-        Navigator.push(
-            context, MaterialPageRoute(builder: (_) => WebViewExample()));
+        Navigator.push(context, MaterialPageRoute(builder: (_) => WebViewExample()));
         break;
       case 3:
-        Navigator.push(
-            context, MaterialPageRoute(builder: (_) => DeviceInfo()));
+        Navigator.push(context, MaterialPageRoute(builder: (_) => DeviceInfo()));
         break;
       case 4:
-        //Navigator.push(context, MaterialPageRoute(builder: (_) => BarChartSample5()));
+        Navigator.push(context, MaterialPageRoute(builder: (_) => ExampleLocalAuth()));
         break;
       case 5:
         //Navigator.push(context, MaterialPageRoute(builder: (_) => LineChartSample1()));
@@ -94,27 +94,6 @@ class GuideOfficialPluginRouteState extends State<GuideOfficialPluginRoute> {
         break;
       case 7:
         //Navigator.push(context, MaterialPageRoute(builder: (_) => LineChartSample3()));
-        break;
-      case 8:
-        // Navigator.push(context, MaterialPageRoute(builder: (_) => LineChartSample4()));
-        break;
-      case 9:
-        //Navigator.push(context, MaterialPageRoute(builder: (_) => LineChartSample5()));
-        break;
-      case 10:
-        // Navigator.push(context, MaterialPageRoute(builder: (_) => LineChartSample6()));
-        break;
-      case 11:
-        //Navigator.push(context, MaterialPageRoute(builder: (_) => LineChartSample7()));
-        break;
-      case 12:
-        // Navigator.push(context, MaterialPageRoute(builder: (_) => PieChartSample1()));
-        break;
-      case 13:
-        //Navigator.push(context, MaterialPageRoute(builder: (_) => PieChartSample2()));
-        break;
-      case 14:
-        //Navigator.push(context, MaterialPageRoute(builder: (_) => PieChartSample3()));
         break;
     }
   }
