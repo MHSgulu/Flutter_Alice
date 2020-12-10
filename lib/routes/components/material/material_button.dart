289983254f75c68929b6f36499dc961223d2299b
Ma <594408294@qq.com>
2020-12-10 17:28:08 +0800

    丰富 MD Button 演示

--- lib/routes/components/material/material_button.dart
+++ lib/routes/components/material/material_button.dart
@@ -1,7 +1,16 @@
 import 'package:alice/widgets/custom/my_appbar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MaterialButtonRoute extends StatelessWidget {
+class MaterialButtonRoute extends StatefulWidget {
+  @override
+  _MaterialButtonRouteState createState() => _MaterialButtonRouteState();
+}
+
+class _MaterialButtonRouteState extends State<MaterialButtonRoute> {
+  final isSelected = <bool>[false, false, false];
+  var isIconSelected = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,63 +18,192 @@ class MaterialButtonRoute extends StatelessWidget {
         label: '按钮',
         onPressedBack: () => Navigator.pop(context),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('之前版本的 MD按钮'),
-          SizedBox(height: 20,),
-          Center(
-            child: FlatButton(
-              onPressed: () {
-
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('切换按钮'),
+            SizedBox(height: 10),
+            ToggleButtons(
+              isSelected: isSelected,
+              onPressed: (index) {
+                // Respond to button selection
+                setState(() {
+                  isSelected[index] = !isSelected[index];
+                });
               },
-              child: Text('FlatButton'),
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('BUTTON 1'),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('BUTTON 2'),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('BUTTON 3'),
+                ),
+              ],
             ),
-          ),
-          Center(
-            child: OutlineButton(
-              onPressed: () {
-
+            SizedBox(height: 10),
+            ToggleButtons(
+              color: Colors.black.withOpacity(0.60),
+              selectedColor: Color(0xFF6200EE),
+              selectedBorderColor: Color(0xFF6200EE),
+              fillColor: Color(0xFF6200EE).withOpacity(0.08),
+              splashColor: Color(0xFF6200EE).withOpacity(0.12),
+              hoverColor: Color(0xFF6200EE).withOpacity(0.04),
+              borderRadius: BorderRadius.circular(4.0),
+              constraints: BoxConstraints(minHeight: 36.0),
+              isSelected: isSelected,
+              onPressed: (index) {
+                // Respond to button selection
+                setState(() {
+                  isSelected[index] = !isSelected[index];
+                });
               },
-              child: Text('OutlineButton'),
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('BUTTON 1'),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('BUTTON 2'),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('BUTTON 3'),
+                ),
+              ],
             ),
-          ),
-          Center(
-            child: RaisedButton(
-              onPressed: () {
-
+            SizedBox(height: 10),
+            ToggleButtons(
+              color: Colors.black.withOpacity(0.60),
+              selectedColor: Color(0xFF6200EE),
+              selectedBorderColor: Color(0xFF6200EE),
+              fillColor: Color(0xFF6200EE).withOpacity(0.08),
+              splashColor: Color(0xFF6200EE).withOpacity(0.12),
+              hoverColor: Color(0xFF6200EE).withOpacity(0.04),
+              borderRadius: BorderRadius.circular(4.0),
+              isSelected: isSelected,
+              onPressed: (index) {
+                // Respond to button selection
+                setState(() {
+                  isSelected[index] = !isSelected[index];
+                });
               },
-              child: Text('RaisedButton'),
+              children: [
+                Icon(Icons.favorite),
+                Icon(Icons.visibility),
+                Icon(Icons.notifications),
+              ],
             ),
-          ),
-          SizedBox(height: 20,),
-          Text('Flutter1.22 新版 MD按钮'),
-          SizedBox(height: 20,),
-          Center(
-            child: TextButton(
+            SizedBox(height: 20),
+            Text('图标按钮'),
+            SizedBox(height: 10),
+            IconButton(
+              icon: Icon(isIconSelected ? Icons.favorite : Icons.favorite_border,color: isIconSelected ? Colors.redAccent : Colors.black26,),
+              color: Colors.white,
               onPressed: () {
-
+                setState(() {
+                  isIconSelected = !isIconSelected;
+                });
               },
-              child: Text('TextButton'),
             ),
-          ),
-          Center(
-            child: OutlinedButton(
-              onPressed: () {
-
-              },
-              child: Text('OutlinedButton'),
+            SizedBox(height: 20),
+            Text('文字、轮廓、包含按钮'),
+            SizedBox(height: 10),
+            Center(
+              child: FlatButton(
+                onPressed: () {},
+                child: Text('FlatButton'),
+              ),
             ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-
-              },
-              child: Text('ElevatedButton'),
+            Center(
+              child: FlatButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('FlatButton'),
+              ),
             ),
-          ),
-        ],
+            Center(
+              child: OutlineButton(
+                onPressed: () {},
+                child: Text('OutlineButton'),
+              ),
+            ),
+            Center(
+              child: OutlineButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('OutlineButton'),
+              ),
+            ),
+            Center(
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('RaisedButton'),
+              ),
+            ),
+            Center(
+              child: RaisedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('RaisedButton'),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Flutter1.22 新版 MD按钮'),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: TextButton(
+                onPressed: () {},
+                child: Text('TextButton'),
+              ),
+            ),
+            Center(
+              child: TextButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('TextButton'),
+              ),
+            ),
+            Center(
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Text('OutlinedButton'),
+              ),
+            ),
+            Center(
+              child: OutlinedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('OutlinedButton'),
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('ElevatedButton'),
+              ),
+            ),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                label: Text('ElevatedButton'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
