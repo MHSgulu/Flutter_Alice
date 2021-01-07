0aea661a1ce75278ff27d35a7e6db2f337d6e726
Ma <594408294@qq.com>
2021-01-07 11:57:44 +0800

    增加 pub演示

---
+++ lib/routes/example/individual/example_flip_panel.dart
@@ -0,0 +1,230 @@
+import 'package:flip_panel/flip_panel.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
+
+class ExampleFlipPanelPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlipPanel'),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text('FlipImage'),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => AnimatedImagePage())),
+          ),
+          ListTile(
+            title: Text('FlipClock'),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => FlipClockPage())),
+          ),
+          ListTile(
+            title: Text('CountdownClock'),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => CountdownClockPage())),
+          ),
+          ListTile(
+            title: Text('DaysToGo'),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => ReverseCountdown())),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnimatedImagePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imageWidth = 320.0;
+    final imageHeight = 171.0;
+    final toleranceFactor = 0.033;
+    final widthFactor = 0.125;
+    final heightFactor = 0.5;
+
+    final random = Random();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlipImage'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  0,
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                ]
+                    .map((count) => FlipPanel.stream(
+                  itemStream: Stream.fromFuture(Future.delayed(
+                      Duration(milliseconds: random.nextInt(20) * 100),
+                          () => 1)),
+                  itemBuilder: (_, value) => value <= 0
+                      ? Container(
+                    color: Colors.white,
+                    width: widthFactor * imageWidth,
+                    height: heightFactor * imageHeight,
+                  )
+                      : ClipRect(
+                      child: Align(
+                          alignment: Alignment(
+                              -1.0 +
+                                  count * 2 * 0.125 +
+                                  count * toleranceFactor,
+                              -1.0),
+                          widthFactor: widthFactor,
+                          heightFactor: heightFactor,
+                          child: Image.asset(
+                            'assets/images/flutter_cover.png',
+                            width: imageWidth,
+                            height: imageHeight,
+                          ))),
+                  initValue: 0,
+                  spacing: 0.0,
+                  direction: FlipDirection.up,
+                ))
+                    .toList(),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  0,
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                ]
+                    .map((count) => FlipPanel.stream(
+                  itemStream: Stream.fromFuture(Future.delayed(
+                      Duration(milliseconds: random.nextInt(20) * 100),
+                          () => 1)),
+                  itemBuilder: (_, value) => value <= 0
+                      ? Container(
+                    color: Colors.white,
+                    width: widthFactor * imageWidth,
+                    height: heightFactor * imageHeight,
+                  )
+                      : ClipRect(
+                      child: Align(
+                          alignment: Alignment(
+                              -1.0 +
+                                  count * 2 * 0.125 +
+                                  count * toleranceFactor,
+                              1.0),
+                          widthFactor: widthFactor,
+                          heightFactor: heightFactor,
+                          child: Image.asset(
+                            'assets/images/flutter_cover.png',
+                            width: imageWidth,
+                            height: imageHeight,
+                          ))),
+                  initValue: 0,
+                  spacing: 0.0,
+                  direction: FlipDirection.down,
+                ))
+                    .toList(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlipClockPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlipClock'),
+      ),
+      body: Center(
+        child: SizedBox(
+          height: 64.0,
+          child: FlipClock.simple(
+            startTime: DateTime.now(),
+            digitColor: Colors.white,
+            backgroundColor: Colors.black,
+            digitSize: 48.0,
+            borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CountdownClockPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlipClock'),
+      ),
+      body: Center(
+        child: SizedBox(
+          height: 64.0,
+          child: FlipClock.countdown(
+            duration: Duration(minutes: 1),
+            digitColor: Colors.white,
+            backgroundColor: Colors.black,
+            digitSize: 48.0,
+            borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+            onDone: () => print('ih'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReverseCountdown extends StatelessWidget {
+  //when using reverse countdown in your own app, change debugMode to false and provide the requied dDay values.
+  final bool debugMode = true;
+  DateTime now = DateTime.now();
+  DateTime dDay = DateTime(2018, 11, 26, 0, 0, 0);
+
+  @override
+  Widget build(BuildContext context) {
+    dDay = (debugMode)
+        ? DateTime(now.year, now.month + 2, now.day, now.hour, now.minute,
+        now.second + 10)
+        : dDay;
+
+    Duration _duration = dDay.difference(now);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ReverseCountdown'),
+      ),
+      body: Center(
+        child: SizedBox(
+          height: 64.0,
+          child: FlipClock.reverseCountdown(
+            duration: _duration,
+            digitColor: Colors.white,
+            backgroundColor: Colors.black,
+            digitSize: 30.0,
+            borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+            //onDone: () => print('ih'),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/routes/example/individual/example_shimmer.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ExampleShimmerPage extends StatefulWidget {
+  @override
+  _ExampleShimmerPageState createState() => _ExampleShimmerPageState();
+}
+
+class _ExampleShimmerPageState extends State<ExampleShimmerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shimmer'),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: const Text('Loading List'),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => LoadingListPage())),
+          ),
+          ListTile(
+            title: const Text('Slide To Unlock'),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => SlideToUnlockPage())),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class LoadingListPage extends StatefulWidget {
+  @override
+  _LoadingListPageState createState() => _LoadingListPageState();
+}
+
+class _LoadingListPageState extends State<LoadingListPage> {
+  bool _enabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Loading List'),
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+              child: Shimmer.fromColors(
+                baseColor: Colors.grey[300],
+                highlightColor: Colors.grey[100],
+                enabled: _enabled,
+                child: ListView.builder(
+                  itemBuilder: (_, __) => Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 48.0,
+                          height: 48.0,
+                          color: Colors.white,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                width: double.infinity,
+                                height: 8.0,
+                                color: Colors.white,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 2.0),
+                              ),
+                              Container(
+                                width: double.infinity,
+                                height: 8.0,
+                                color: Colors.white,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 2.0),
+                              ),
+                              Container(
+                                width: 40.0,
+                                height: 8.0,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  itemCount: 6,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      _enabled = !_enabled;
+                    });
+                  },
+                  child: Text(
+                    _enabled ? 'Stop' : 'Play',
+                    style: Theme.of(context).textTheme.button.copyWith(
+                        fontSize: 18.0,
+                        color: _enabled ? Colors.redAccent : Colors.green),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SlideToUnlockPage extends StatelessWidget {
+  final List<String> days = <String>[
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  final List<String> months = <String>[
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final DateTime time = DateTime.now();
+    final int hour = time.hour;
+    final int minute = time.minute;
+    final int day = time.weekday;
+    final int month = time.month;
+    final int dayInMonth = time.day;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Slide To Unlock'),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/images/background.jpg',
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 48.0,
+            right: 0.0,
+            left: 0.0,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    '${hour < 10 ? '0$hour' : '$hour'}:${minute < 10 ? '0$minute' : '$minute'}',
+                    style: TextStyle(
+                      fontSize: 60.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                  ),
+                  Text(
+                    '${days[day - 1]}, ${months[month - 1]} $dayInMonth',
+                    style: TextStyle(fontSize: 24.0, color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 24.0,
+              left: 0.0,
+              right: 0.0,
+              child: Center(
+                child: Opacity(
+                  opacity: 0.8,
+                  child: Shimmer.fromColors(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/images/chevron_right.png',
+                          height: 20.0,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                        ),
+                        const Text(
+                          'Slide to unlock',
+                          style: TextStyle(
+                            fontSize: 28.0,
+                          ),
+                        )
+                      ],
+                    ),
+                    baseColor: Colors.black12,
+                    highlightColor: Colors.white,
+                    loop: 3,
+                  ),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
