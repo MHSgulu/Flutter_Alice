fc63253d44f04d81100c7eb6653e9a4d530a1739
Ma <594408294@qq.com>
2021-01-19 17:41:52 +0800

    常规修改

--- lib/routes/movie/home/movie_carousel.dart
+++ lib/routes/movie/home/movie_carousel.dart
@@ -1,17 +1,18 @@
+import 'package:alice/widgets/custom/my_fade_in_image.dart';
+import 'package:alice/widgets/custom/my_rounded_rectang_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
-
-class MovieCarouselView extends StatefulWidget{
+class MovieCarouselView extends StatefulWidget {
   @override
   _MovieCarouselViewState createState() => _MovieCarouselViewState();
 }
 
 class _MovieCarouselViewState extends State<MovieCarouselView> {
   List<String> bannerImgUrl = [
-    'https://img3.doubanio.com/view/photo/l/public/p2552055511.webp',
-    'https://img1.doubanio.com/view/photo/m/public/p2561712948.webp',
-    'https://img9.doubanio.com/view/photo/l/public/p2576400566.webp',
+    'https://img9.doubanio.com/view/photo/l/public/p2167320929.webp', //黄金时代
+    'https://img9.doubanio.com/view/photo/l/public/p2535265164.webp', //影
+    'https://img9.doubanio.com/view/photo/l/public/p2540924496.webp', //龙猫
   ];
 
   @override
@@ -21,11 +22,11 @@ class _MovieCarouselViewState extends State<MovieCarouselView> {
       child: SizedBox(
         height: 150,
         child: Swiper(
-          autoplay: true,  //自动播放开关.
+          autoplay: true, //自动播放开关.
           //autoplayDelay: 3000,  //自动播放延迟毫秒数. 默认3秒
           itemCount: bannerImgUrl.length,
-          viewportFraction: 0.85,  //当前轮播图的视口大小
-          scale: 0.95,  //两边轮播图的视口距离中间视口的距离 值越大越近
+          viewportFraction: 0.85, //当前轮播图的视口大小
+          scale: 0.95, //两边轮播图的视口距离中间视口的距离 值越大越近
           pagination: SwiperPagination(
             builder: DotSwiperPaginationBuilder(
               size: 5,
@@ -34,13 +35,11 @@ class _MovieCarouselViewState extends State<MovieCarouselView> {
               activeColor: Colors.teal,
             ),
           ),
-          itemBuilder: (BuildContext context,int index){
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadiusDirectional.circular(4.0),
+          itemBuilder: (BuildContext context, int index) {
+            return MyRRectCard(
+              child: MyFadeInImage(
+                imageUrl: '${bannerImgUrl[index]}',
               ),
-              clipBehavior: Clip.antiAlias,
-              child: Image.network(bannerImgUrl[index],fit: BoxFit.cover),
             );
           },
         ),
