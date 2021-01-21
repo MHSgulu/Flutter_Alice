931f9c1ae133001b912091d6413963a681a4ddba
Ma <594408294@qq.com>
2021-01-21 11:22:40 +0800

    封装 lato字体

---
+++ lib/widgets/custom/lato_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LatoText extends StatelessWidget {
+  final String data;
+  final TextStyle style;
+
+  const LatoText({
+    Key key,
+    this.data,
+    this.style,
+  })  : assert(
+          data != null,
+          '必须为文本窗口小部件提供非空字符串',
+        ),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data,
+      style: GoogleFonts.lato(
+        textStyle: style ?? TextStyle(),
+      ),
+    );
+  }
+}
