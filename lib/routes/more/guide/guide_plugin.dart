a1b809cf8ca6d1cf47d36727df194d970d5cfaaa
Ma <594408294@qq.com>
2021-02-02 17:41:53 +0800

    确保能打正式包

--- lib/routes/more/guide/guide_plugin.dart
+++ lib/routes/more/guide/guide_plugin.dart
@@ -1,5 +1,4 @@
 import 'package:alice/routes/example/plugin/baidu_map/location/example_baidu_location.dart';
-import 'package:alice/routes/other/zego/zego_main.dart';
 import 'package:alice/widgets/custom/custom_scroll_behavior.dart';
 import 'package:alice/widgets/custom/my_appbar.dart';
 import 'package:flutter/material.dart';
@@ -19,29 +18,6 @@ class GuidePlugin extends StatelessWidget {
             padding: EdgeInsets.all(8.0),
             child: Column(
               children: <Widget>[
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusDirectional.circular(4),
-                  ),
-                  clipBehavior: Clip.antiAlias,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ZegoLiveHomePage()));
-                    },
-                    splashColor: Colors.blueGrey[100],
-                    child: ListTile(
-                      leading: Image.asset(
-                        'assets/icons/icon_live.png',
-                        width: 30,
-                        height: 30,
-                      ),
-                      title: Text('即构实时音频/视频'),
-                    ),
-                  ),
-                ),
                 Card(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadiusDirectional.circular(4),
