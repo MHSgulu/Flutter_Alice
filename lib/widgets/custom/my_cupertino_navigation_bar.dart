e137d90ce421de0f139bacba3d39c52bb5938c32
Ma <594408294@qq.com>
2021-02-04 14:54:28 +0800

    增加 自定义类

---
+++ lib/widgets/custom/my_cupertino_navigation_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+class MyCupertinoNavigationBar extends StatefulWidget {
+  @override
+  _MyCupertinoNavigationBarState createState() => _MyCupertinoNavigationBarState();
+}
+
+class _MyCupertinoNavigationBarState extends State<MyCupertinoNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
