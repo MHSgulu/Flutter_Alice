92c1f3fae460686ad8fbbd1f41e8589cea6f7ebc
Ma <594408294@qq.com>
2021-02-19 15:40:14 +0800

    更换热映电影数据

--- lib/common/const/constant.dart
+++ lib/common/const/constant.dart
@@ -6,52 +6,34 @@ class Constant {
 
   static final List<Movie> movieDataList = [
     Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2630334695.webp',
-        '吉祥如意',
-        8.1),
+      'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2629056068.webp',
+      '你好，李焕英',
+      8.2,
+    ),
     Movie(
-        'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2618247457.webp',
-        '送你一朵小红花',
-        7.5),
+      'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2622388983.webp',
+      '唐人街探案3',
+      5.7,
+    ),
     Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2630378196.webp',
-        '蜂鸟计划',
-        7.1),
+      'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2628440875.webp',
+      '刺杀小说家',
+      7.0,
+    ),
     Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2631409396.webp',
-        '温泉屋的小老板娘',
-        7.2),
+      'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2632862664.webp',
+      '人潮汹涌',
+      7.2,
+    ),
     Movie(
-        'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2630893317.webp',
-        '大红包',
-        4.7),
+      'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2631711326.webp',
+      '新神榜：哪吒重生',
+      7.4,
+    ),
     Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2631157729.webp',
-        '小伟',
-        7.8),
-    Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2626308994.webp',
-        '心灵奇旅',
-        8.9),
-    Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2629413230.webp',
-        '缉魂',
-        7.1),
-    Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2621379901.webp',
-        '拆弹专家2',
-        7.9),
-    Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2618446242.webp',
-        '温暖的抱抱',
-        5.6),
-    Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2627025706.webp',
-        '紧急救援',
-        6.3),
-    Movie(
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2624607255.webp',
-        '疯狂原始人2',
-        8.0),
+      'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2629260713.webp',
+      '侍神令',
+      5.9,
+    ),
   ];
 }
